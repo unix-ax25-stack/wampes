99fef8f5161120cf5ac0e95ec296c2af18c57cd4
deyke <deyke>
1991-04-17 19:47:05 +0000

    *** empty log message ***

--- src/n8250.c
+++ src/n8250.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/n8250.c,v 1.3 1991/04/12 18:34:28 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/n8250.c,v 1.4 1991/04/17 19:47:05 deyke Exp $ */
 
 #include <sys/types.h>
 
@@ -63,7 +63,7 @@ int  dev;
       ap->fd = -1;
       return (-1);
     }
-    ap->speed = 0;
+    ap->speed = -1;
   } else {
     strcpy(buf, "/dev/");
     strcat(buf, ifp->name);
