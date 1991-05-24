e7b77081063b8b012578992652ec5e97b4464f6b
deyke <deyke>
1991-05-24 12:09:15 +0000

    *** empty log message ***

--- src/icmpcmd.c
+++ src/icmpcmd.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/icmpcmd.c,v 1.8 1991/04/12 18:34:54 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/icmpcmd.c,v 1.9 1991/05/24 12:09:45 deyke Exp $ */
 
 /* ICMP-related user commands
  * Copyright 1991 Phil Karn, KA9Q
@@ -271,7 +271,7 @@ int16 len;      /* Length of data field */
 		free_p(data);
 		return 0;
 	}
-	return ip_send(Ip_addr,target,ICMP_PTCL,0,0,bp,len,0,0);
+	return ip_send(INADDR_ANY,target,ICMP_PTCL,0,0,bp,len,0,0);
 }
 static int16
 hash_ping(dest)
