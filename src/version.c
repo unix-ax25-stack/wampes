57487a47cc1aae782153e22d3f06fa444a89849f
deyke <deyke>
1991-07-24 13:23:19 +0000

    *** empty log message ***

--- src/version.c
+++ src/version.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.14 1991/07/17 15:00:04 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.15 1991/07/24 13:23:19 deyke Exp $ */
 
 /* version control information */
-char Version[] = "WAMPES-910717";
+char Version[] = "WAMPES-910724";
