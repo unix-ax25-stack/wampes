6b9c863737be6015c768190c6a36dfa681291819
deyke <deyke>
1991-10-03 11:04:56 +0000

    *** empty log message ***

--- src/tty.h
+++ src/tty.h
@@ -1,9 +1,16 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/tty.h,v 1.1 1991/04/25 18:28:46 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/tty.h,v 1.2 1991/10/03 11:05:24 deyke Exp $ */
 
 #ifndef _TTY_H
 #define _TTY_H
 
+#define NUM_FKEY        10      /* Number of function keys */
+
+/* In main.c: */
+extern char Escape;
+
 /* In ttydriv.c: */
+extern char *Fkey_table[];
+extern char *Fkey_ptr;
 int raw __ARGS((void));
 int cooked __ARGS((void));
 int ttydriv __ARGS((int chr, char **buf));
