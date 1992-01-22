25ed27c14b46af641afc9d1a1bcfb14e425d3e0f
deyke <deyke>
1992-01-22 11:12:01 +0000

    *** empty log message ***

--- src/n8250.c
+++ src/n8250.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/n8250.c,v 1.13 1992/01/20 17:17:27 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/n8250.c,v 1.14 1992/01/22 11:12:01 deyke Exp $ */
 
 #include <sys/types.h>
 
@@ -235,7 +235,6 @@ struct iface *ifp;
 		close(ap->fd);
 		ap->fd = -1;
 	}
-	ap->iface = NULLIF;
 
 	return 0;
 }
--- src/version.c
+++ src/version.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.28 1992/01/20 17:18:12 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.29 1992/01/22 11:12:50 deyke Exp $ */
 
 /* version control information */
-char Version[] = "WAMPES-920120";
+char Version[] = "WAMPES-920122";
