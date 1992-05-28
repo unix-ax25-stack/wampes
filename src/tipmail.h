318f566df661943f25f468f2d04fe0deaeaa75ff
deyke <deyke>
1992-05-28 13:50:57 +0000

    Initial revision

---
+++ src/tipmail.h
@@ -0,0 +1,6 @@
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/tipmail.h,v 1.1 1992/05/28 13:50:57 deyke Exp $ */
+
+int tipstart __ARGS((int argc,char *argv[],void *p));
+int tip0 __ARGS((int argc,char *argv[],void *p));
+int telnet0 __ARGS((int argc,char *argv[],void *p));
+int telnet1 __ARGS((int argc,char *argv[],void *p));
