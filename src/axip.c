da9424ff00ef8fa7b2d496b78ca8b6dd7f956575
deyke <deyke>
1992-08-19 13:20:20 +0000

    *** empty log message ***

--- src/axip.c
+++ src/axip.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/axip.c,v 1.10 1992/08/11 21:32:55 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/axip.c,v 1.11 1992/08/19 13:20:23 deyke Exp $ */
 
 #include "global.h"
 
@@ -295,7 +295,7 @@ void *p;
   }
 
   if (type == USE_UDP) {
-    memset(&addr, 0, sizeof(addr));
+    memset((char *) &addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(port);
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.43 1992/08/11 21:32:21 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.44 1992/08/19 13:20:41 deyke Exp $ */
 
 #ifndef __LINT__
-static char id[] = "@(#)WAMPES-920811";
+static char id[] = "@(#)WAMPES-920819";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-920811";
+char Version[] = "WAMPES-920819";
