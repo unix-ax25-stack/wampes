077edcef9db5315c3d0eed8a2c400c70a89cb570
deyke <deyke>
1993-04-11 07:05:41 +0000

    *** empty log message ***

--- src/hpux.c
+++ src/hpux.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/hpux.c,v 1.35 1993/03/30 17:24:01 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/hpux.c,v 1.36 1993/04/11 07:06:32 deyke Exp $ */
 
 #include <sys/types.h>
 
@@ -128,7 +128,6 @@ void ioinit()
     fopen("/dev/null", "r+");
     fopen("/dev/null", "r+");
     fopen("/dev/null", "r+");
-    remote_kbd_initialize();
   }
   umask(022);
   signal(SIGPIPE, SIG_IGN);
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.83 1993/04/06 13:13:57 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.84 1993/04/11 07:06:37 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-930406";
+static char id[] = "@(#)WAMPES-930411";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-930406";
+char Version[] = "WAMPES-930411";
