5e1e2bb28b35e79e52f71f6a85ddff7a51c6b203
deyke <deyke>
1993-06-10 09:48:12 +0000

    Initial revision

---
+++ lib/strdup.h
@@ -0,0 +1,9 @@
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/lib/strdup.h,v 1.1 1993/06/10 09:48:12 deyke Exp $ */
+
+#ifndef _STRDUP_H
+#define _STRDUP_H
+
+/* In strdup.c: */
+char *strdup();
+
+#endif  /* _STRDUP_H */
