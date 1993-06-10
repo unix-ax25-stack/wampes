980efe8d723af4a467477493c79d65d3f8a4e22f
deyke <deyke>
1993-06-10 09:43:21 +0000

    *** empty log message ***

--- lib/strdup.c
+++ lib/strdup.c
@@ -1,8 +1,11 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/lib/strdup.c,v 1.1 1993/06/06 08:25:45 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/lib/strdup.c,v 1.2 1993/06/10 09:46:32 deyke Exp $ */
+
+#include <stdlib.h>
 
 extern char *strcpy();
 extern int strlen();
-extern void *malloc();
+
+#include "strdup.h"
 
 char *strdup(const char *s)
 {
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.97 1993/06/07 06:31:06 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.98 1993/06/10 09:43:55 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-930607";
+static char id[] = "@(#)WAMPES-930610";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-930607";
+char Version[] = "WAMPES-930610";
