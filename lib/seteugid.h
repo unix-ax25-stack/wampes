175aa4fcba43eb9b3a937ae4ca6bbf5fa540590a
deyke <deyke>
1994-01-21 11:12:48 +0000

    Initial revision

---
+++ lib/seteugid.h
@@ -0,0 +1,9 @@
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/lib/seteugid.h,v 1.1 1994/01/21 11:12:48 deyke Exp $ */
+
+#ifndef _SETEUGID_H
+#define _SETEUGID_H
+
+/* In seteugid.c: */
+void seteugid(int uid, int gid);
+
+#endif  /* _SETEUGID_H */
