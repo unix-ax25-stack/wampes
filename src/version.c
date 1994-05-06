4888db589ebf052ec18f7d0d4175f0ee4c58b84a
deyke <deyke>
1994-05-06 17:15:28 +0000

    *** empty log message ***

--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.198 1994/05/05 11:18:05 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.199 1994/05/06 17:16:03 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-940505";
+static char id[] = "@(#)WAMPES-940506";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-940505";
+char Version[] = "WAMPES-940506";
