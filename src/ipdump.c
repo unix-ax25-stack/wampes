1edfe962969896e6ff8f4ce6bd7af5c6413787e7
deyke <deyke>
1994-07-13 15:48:16 +0000

    *** empty log message ***

--- src/ipdump.c
+++ src/ipdump.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/ipdump.c,v 1.7 1993/05/17 13:44:59 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/ipdump.c,v 1.8 1994/07/13 15:48:37 deyke Exp $ */
 
 /* IP header tracing routines
  * Copyright 1991 Phil Karn, KA9Q
@@ -68,6 +68,7 @@ int check;
 		return;
 	}
 	switch(uchar(ip.protocol)){
+	case IP4_PTCL:
 	case IP_PTCL:
 		fprintf(fp," prot IP\n");
 		ip_dump(fp,bpp,check);
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.210 1994/07/12 16:30:06 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.211 1994/07/13 15:48:44 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-940712";
+static char id[] = "@(#)WAMPES-940713";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-940712";
+char Version[] = "WAMPES-940713";
