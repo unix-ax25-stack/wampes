f6a2a708314b8f23c77e3bbf0a61598d5ce7e010
deyke <deyke>
1994-07-18 09:15:07 +0000

    *** empty log message ***

--- src/ipip.c
+++ src/ipip.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/ipip.c,v 1.11 1994/04/13 09:51:44 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/ipip.c,v 1.12 1994/07/18 09:15:24 deyke Exp $ */
 
 #include <sys/types.h>
 
@@ -122,7 +122,7 @@ int ipip_attach(int argc, char *argv[], void *p)
 
   char *ifname = "ipip";
   int fd;
-  int port = IP_PTCL;
+  int port = IP4_PTCL;
   int type = USE_IP;
   struct edv_t *edv;
   struct iface *ifp;
