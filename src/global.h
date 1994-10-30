4163127c4b9fc793670527b2530d494a87068dd6
deyke <deyke>
1994-10-30 21:26:40 +0000

    *** empty log message ***

--- src/global.h
+++ src/global.h
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/global.h,v 1.34 1994/10/21 11:54:18 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/global.h,v 1.35 1994/10/30 21:26:58 deyke Exp $ */
 
 #ifndef _GLOBAL_H
 #define _GLOBAL_H
@@ -301,6 +301,7 @@ struct asy;
 struct ax25_cb;
 struct iface;
 struct mbuf;
+struct session;
 struct usock;
 
 extern int Debug;
