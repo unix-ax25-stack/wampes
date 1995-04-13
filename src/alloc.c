9126f2336c0ac196b1312026ff4fe81df1e45f5c
deyke <deyke>
1995-04-13 14:29:57 +0000

    *** empty log message ***

--- src/alloc.c
+++ src/alloc.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/alloc.c,v 1.26 1994/10/06 16:15:18 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/alloc.c,v 1.27 1995/04/13 14:30:14 deyke Exp $ */
 
 /* memory allocation routines
  */
@@ -11,6 +11,8 @@
 #define calloc  Xcalloc
 
 #endif
+
+#define NO_FIX_MALLOC
 
 #include <stdio.h>
 #include <stdlib.h>
