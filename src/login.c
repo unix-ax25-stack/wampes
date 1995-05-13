9412bbf87f2a04d55b8d1c1950ad3f786c8efdeb
deyke <deyke>
1995-05-13 18:46:45 +0000

    *** empty log message ***

--- src/login.c
+++ src/login.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/login.c,v 1.57 1995/03/24 13:00:05 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/login.c,v 1.58 1995/05/13 18:46:51 deyke Exp $ */
 
 #include <sys/types.h>
 
@@ -666,7 +666,7 @@ void login_close(struct login_cb *tp)
 
 /*---------------------------------------------------------------------------*/
 
-#define ASIZE 512
+#define ASIZE 480
 
 #define add_to_mbuf(chr) \
 { \
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.270 1995/05/09 21:13:03 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.271 1995/05/13 18:46:54 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-950509";
+static char id[] = "@(#)WAMPES-950513";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-950509";
+char Version[] = "WAMPES-950513";
