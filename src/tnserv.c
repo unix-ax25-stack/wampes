9dda49717be8316e5b3e53818e971aafcf8b5bdd
deyke <deyke>
1995-12-26 11:18:37 +0000

    *** empty log message ***

--- src/tnserv.c
+++ src/tnserv.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/tnserv.c,v 1.12 1995/12/20 09:46:57 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/tnserv.c,v 1.13 1995/12/26 11:18:47 deyke Exp $ */
 
 #include "global.h"
 #include "mbuf.h"
@@ -17,8 +17,9 @@ static void tnserv_recv_upcall(struct tcb *tcb, int32 cnt)
   struct mbuf *bp;
 
   if (tcb->user) {
+    bp = 0;
     recv_tcp(tcb, &bp, 0);
-    login_write((struct login_cb *) tcb->user, bp);
+    login_write((struct login_cb *) tcb->user, &bp);
   }
 }
 
