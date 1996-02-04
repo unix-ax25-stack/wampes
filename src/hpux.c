c22cb28bbf69cb0ea26f0040028c1b264e032315
deyke <deyke>
1996-02-04 11:17:32 +0000

    *** empty log message ***

--- src/hpux.c
+++ src/hpux.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/hpux.c,v 1.57 1996/01/22 13:13:39 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/hpux.c,v 1.58 1996/02/04 11:17:38 deyke Exp $ */
 
 #include <sys/types.h>
 
@@ -44,7 +44,7 @@
 
 #define TIMEOUT 120
 
-#ifdef __hpux
+#if defined __hpux && !defined _FD_SET
 #define SEL_ARG(x) ((int *) (x))
 #else
 #define SEL_ARG(x) (x)
--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
-/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.310 1996/01/30 14:45:05 deyke Exp $ */
+/* @(#) $Header: /Users/thomas/direwolf-libax25-agwpe/wampes-import/wampes-cvs/wampes/src/version.c,v 1.311 1996/02/04 11:17:44 deyke Exp $ */
 
-static char id[] = "@(#)WAMPES-960130";
+static char id[] = "@(#)WAMPES-960204";
 
 /* version control information */
 char *Version = id + 4;
